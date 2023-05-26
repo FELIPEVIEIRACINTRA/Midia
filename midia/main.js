3d0e51f5f47c27f3e45f3fd6a1b6b78d81ad1ea8
Felipe Vieira Cintra <92827339+FELIPEVIEIRACINTRA@users.noreply.github.com>
2023-05-26 16:06:08 -0300

    Atualização

--- midia/main.js
+++ midia/main.js
@@ -1,26 +1,36 @@
-
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
-
-//enquanto
-while (contador < listaDeTeclas.length) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-//template String
-    const idAudio = `#som_${instrumento}`;
-   // console.log(idAudio);    
-
-    tecla.onclick = function() {
-        tocaSom(idAudio);
-    };
-
-    contador = contador + 1;
-
-    //console.log(contador);
-}
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+ 
+    if (elemento && elemento.localName === 'audio') {
+         elemento.play();
+    }
+    else {
+         console.log('Elemento não encontrado ou seletor inválido');
+    }
+ }
+ 
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+ 
+ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+ 
+     const tecla = listaDeTeclas[contador];
+     const instrumento = tecla.classList[1];
+     const idAudio = `#som_${instrumento}`; //templete string
+ 
+     tecla.onclick = function() {
+         tocaSom(idAudio);
+     }
+     
+     tecla.onkeydown = function (evento) {
+ 
+         console.log(evento.code == 'Space')
+         if (evento.code === 'Space' || evento.code === 'Enter') {
+             tecla.classList.add('ativa');
+         }
+         
+     }
+ 
+     tecla.onkeyup = function () {
+         tecla.classList.remove('ativa');
+     }
+ }
